4bcea16996d5f506f40afa5ab24849606a8cecf9
thanhbao0390 <46698469+thanhbao0390@users.noreply.github.com>
2019-11-01 10:23:45 +0700

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,37 @@
+<?php
+$to      = 'thanhbao0390@gmail.com';
+$subject = 'sample test';
+$message = 'hello';
+$headers = 'From: thanhbao0390@gmail.com' . "\r\n" .
+    'Reply-To: thanhbao0390@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<?php include 'var.php';?>
+<?php include 'header.php';?>
+
+<body class="home page-template page-template-templates page-template-home page-template-templateshome-php page page-id-2 wp-custom-logo group-blog global-layout-right-sidebar blog-layout-list global-sticky-sidebar">
+    <div id="page" class="site">
+        <?php include 'menu.php';?>
+        <div id="content" class="site-content">
+            <div class="container">
+                <div class="inner-wrapper">
+                    <div id="primary" class="content-area">
+                        <main id="main" class="site-main" role="main">
+                            <?php include 'submit-body.php';?>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container"></div>
+        <?php include 'footer.php';?>
+    </div>
+    <?php include 'phone-call.php';?>
+  
+</body>
+
+</html>
